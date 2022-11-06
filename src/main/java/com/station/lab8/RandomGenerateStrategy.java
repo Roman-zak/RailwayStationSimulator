a265d4b192a8037ef606490e3c5cac4e2fbb98f7
Svyatoslav Andrushchak <sandrushchak@shift7digital.com>
2022-11-06 21:45:52 +0200

    Implemented RandomGenerateStrategy

--- src/main/java/com/station/lab8/RandomGenerateStrategy.java
+++ src/main/java/com/station/lab8/RandomGenerateStrategy.java
@@ -1,11 +1,18 @@
 package com.station.lab8;
 
-import org.apache.commons.lang3.NotImplementedException;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomGenerateStrategy implements Strategy {
+
+    private int lowerBound;
+    private int upperBound;
+
+    public RandomGenerateStrategy(int lowerBound, int upperBound){
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+    }
     @Override
     public int getInterval() {
-        throw new NotImplementedException();
-       // return 0; //random generate interval
+        return ThreadLocalRandom.current().nextInt(this.lowerBound, this.upperBound + 1);
     }
 }
