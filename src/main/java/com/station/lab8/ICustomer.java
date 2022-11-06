5cc3f1d18b0ec26da0880ed599fba0399cf69e0e
Stepan Halaiko <stepanhalaiko@gmail.com>
2022-11-06 20:19:50 +0000

    Stepan methods

--- src/main/java/com/station/lab8/ICustomer.java
+++ src/main/java/com/station/lab8/ICustomer.java
@@ -2,4 +2,6 @@ package com.station.lab8;
 
 public interface ICustomer {
     void setTicketsCount(int ticketsCount);
+    void setStatus(CustomerStatus status);
+    void setEntrance(Entrance entrance);
 }
--- src/main/java/com/station/lab8/QueueResolver.java
+++ src/main/java/com/station/lab8/QueueResolver.java
@@ -9,4 +9,24 @@ public class QueueResolver implements IQueueResolver {
     public void appointCustomerToQueue(List<ICashRegister> cashRegisters, ICustomer customer) {
         throw new NotImplementedException();
     }
+    public static Position GetPosition(List<CashRegister> cashRegisterList,Position start){
+        Position bestPosition = new Position();
+        CashRegister temp = new CashRegister();
+        temp = cashRegisterList.get(0);
+        for (CashRegister cr:cashRegisterList) {
+            if (cr.getQueue().size()==0){
+                bestPosition = cr.getPosition();
+                break;
+            }
+            else if(cr.getQueue().size()<temp.getQueue().size()){
+                bestPosition = cr.getPosition();
+                temp = cr;
+            } else if (cr.getQueue().size()==temp.getQueue().size()) {
+                bestPosition = cr.getPosition().Distance(start)<temp.getPosition().Distance(start)?cr.getPosition():temp.getPosition();
+            }
+        }
+
+        return bestPosition;
+    }
+
 }
