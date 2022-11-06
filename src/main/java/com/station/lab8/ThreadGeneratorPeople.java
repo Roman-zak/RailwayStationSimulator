c1975c2b2507ceca7bbed2cdbd4e763ad0290a83
Svyatoslav Andrushchak <sandrushchak@shift7digital.com>
2022-11-07 00:17:38 +0200

    Implemented ThreadGeneratorPeople

--- src/main/java/com/station/lab8/ThreadGeneratorPeople.java
+++ src/main/java/com/station/lab8/ThreadGeneratorPeople.java
@@ -2,24 +2,46 @@ package com.station.lab8;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ThreadGeneratorPeople implements IThreadGeneratorPeople {
     private Strategy generatorStrategy;
+    private Station station;
+    private List<Entrance> entranceList;
+
+    public ThreadGeneratorPeople(Strategy generatorStrategy, Station station, List<Entrance> entranceList) {
+        this.generatorStrategy = generatorStrategy;
+        this.station = station;
+        this.entranceList = entranceList;
+    }
 
     @Override
     public void setStrategy(Strategy strategy) {
-       throw new NotImplementedException();
+       this.generatorStrategy = strategy;
     }
 
     @Override
     public void generate() {
-        throw new NotImplementedException();
-
+        int ticketsCount = ThreadLocalRandom.current().nextInt(1, 11);
+        Entrance entrance = this.entranceList.get(ThreadLocalRandom.current().nextInt(0, this.entranceList.size() + 1) - 1);
+        CustomerStatus status = CustomerStatus.values()[new Random().nextInt(CustomerStatus.values().length)];
+        this.station.updateQueues(new Customer(ticketsCount, status, entrance));
     }
 
     @Override
     public void run() {
-        //call generate()
-        throw new NotImplementedException();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                generate();
+            }
+        };
+        Timer timer = new Timer("PeopleGeneratorTimer");
 
+        long delay = this.generatorStrategy.getInterval();
+        timer.schedule(task, delay);
     }
 }
