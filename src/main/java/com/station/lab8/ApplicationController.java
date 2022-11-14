047307c41d5474aff3eae74013acaad23ec7274f
Saddie <saddie.yy@gmail.com>
2022-11-14 22:50:03 +0200

    added views and fixes

--- src/main/java/com/station/lab8/ApplicationController.java
+++ src/main/java/com/station/lab8/ApplicationController.java
@@ -1,68 +1,115 @@
 package com.station.lab8;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class ApplicationController implements Initializable {
-    @FXML ComboBox<Integer> countOfCashRegisters;
-    @FXML ComboBox<Integer> countOfEntrances;
-    @FXML TableView<CashRegisterWrapper> cashRegister1;
-    @FXML TableView<CashRegisterWrapper> cashRegister2;
-    @FXML TableView<CashRegisterWrapper> cashRegister3;
-    @FXML TableView<CashRegisterWrapper> cashRegister4;
-    @FXML TableView<CashRegisterWrapper> cashRegister5;
-    @FXML TableView<CashRegisterWrapper> cashRegisterSpare;
-    @FXML Label labelCashRegister1;
-    @FXML Label labelCashRegister2;
-    @FXML Label labelCashRegister3;
-    @FXML Label labelCashRegister4;
-    @FXML Label labelCashRegister5;
-    @FXML Label labelCashRegisterSpare;
-    @FXML TextField minServiceTime;
-    @FXML TextField maxServiceTime;
-    @FXML TextField cashRegisterDisconnection;
-    @FXML TableView<EntranceWrapper> tableEntrances;
-    @FXML TableColumn<EntranceWrapper, String> entrancePositionX;
-    @FXML TableColumn<EntranceWrapper, String> entrancePositionY;
-
+    @FXML
+    ComboBox<Integer> countOfCashRegisters;
+    @FXML
+    ComboBox<Integer> countOfDisconnect;
+    @FXML
+    ComboBox<Integer> countOfEntrances;
+    @FXML
+    TableView<CashRegisterWrapper> cashRegister1;
+    @FXML
+    TableView<CashRegisterWrapper> cashRegister2;
+    @FXML
+    TableView<CashRegisterWrapper> cashRegister3;
+    @FXML
+    TableView<CashRegisterWrapper> cashRegister4;
+    @FXML
+    TableView<CashRegisterWrapper> cashRegister5;
+    @FXML
+    TableView<CashRegisterWrapper> cashRegisterSpare;
+    @FXML
+    Label labelCashRegister1;
+    @FXML
+    Label labelCashRegister2;
+    @FXML
+    Label labelCashRegister3;
+    @FXML
+    Label labelCashRegister4;
+    @FXML
+    Label labelCashRegister5;
+    @FXML
+    Label labelCashRegisterSpare;
+    @FXML
+    TextField minServiceTime;
+    @FXML
+    TextField maxServiceTime;
+    @FXML
+    TableView<EntranceWrapper> tableEntrances;
+    @FXML
+    TableColumn<EntranceWrapper, String> entrancePositionX;
+    @FXML
+    TableColumn<EntranceWrapper, String> entrancePositionY;
     ObservableList<Integer> valueOfComboBox = FXCollections.observableArrayList(1, 2, 3, 4, 5);
+    ObservableList<Integer> valueOfDisconnect = FXCollections.observableArrayList();
     ObservableList<EntranceWrapper> entrances = FXCollections.observableArrayList(
-            new EntranceWrapper("0","0"),
-            new EntranceWrapper("1","2")
     );
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         entrancePositionX.setCellValueFactory(new PropertyValueFactory<EntranceWrapper, String>("posX"));
+        entrancePositionX.setCellFactory(TextFieldTableCell.forTableColumn());
+        entrancePositionX.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<EntranceWrapper, String>>() {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent<EntranceWrapper, String> t) {
+                        ((EntranceWrapper) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())
+                        ).setPosX(t.getNewValue());
+                    }
+                }
+        );
         entrancePositionY.setCellValueFactory(new PropertyValueFactory<EntranceWrapper, String>("posY"));
+        entrancePositionY.setCellFactory(TextFieldTableCell.forTableColumn());
+        entrancePositionY.setOnEditCommit(
+                new EventHandler<TableColumn.CellEditEvent<EntranceWrapper, String>>() {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent<EntranceWrapper, String> t) {
+                        ((EntranceWrapper) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())
+                        ).setPosY(t.getNewValue());
+                    }
+                }
+        );
         tableEntrances.setItems(entrances);
 
         countOfCashRegisters.setItems(valueOfComboBox);
         countOfEntrances.setItems(valueOfComboBox);
+        countOfDisconnect.setItems(valueOfDisconnect);
     }
 
-    public void handleChangeCountOfEntrances(ActionEvent event){
-        //need change count of rows in table with cash registers
-        EntranceWrapper entrance = new EntranceWrapper("10", "14");
-        //tableEntrances.getItems().add(entrance);
+    public void handleChangeCountOfEntrances(ActionEvent event) {
+        while (entrances.size() < countOfEntrances.getValue()) {
+            entrances.add(new EntranceWrapper("0", "0"));
+        }
 
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setContentText("You picked " +  countOfEntrances.getValue());
-        alert.showAndWait();
+        while (entrances.size() > countOfEntrances.getValue()) {
+            entrances.remove(entrances.size() - 1);
+        }
+
+
     }
 
-    public void handleChangeCountOfCashRegisters(ActionEvent event){
+    public void handleChangeCountOfCashRegisters(ActionEvent event) {
         //need change count of rows in table with cash register
 
         Integer count = countOfCashRegisters.getValue();
-        
+
         this.cashRegister1.setVisible(true);
         this.cashRegister2.setVisible(true);
         this.cashRegister3.setVisible(true);
@@ -74,33 +121,44 @@ public class ApplicationController implements Initializable {
         this.labelCashRegister4.setVisible(true);
         this.labelCashRegister5.setVisible(true);
 
-        if(count < 5) {
+        if (count < 5) {
             this.cashRegister5.setVisible(false);
             this.labelCashRegister5.setVisible(false);
         }
-        if(count < 4) {
+        if (count < 4) {
             this.cashRegister4.setVisible(false);
             this.labelCashRegister4.setVisible(false);
         }
-        if(count < 3) {
+        if (count < 3) {
             this.cashRegister3.setVisible(false);
             this.labelCashRegister3.setVisible(false);
         }
-        if(count < 2) {
+        if (count < 2) {
             this.cashRegister2.setVisible(false);
             this.labelCashRegister2.setVisible(false);
         }
+
+        while (valueOfDisconnect.size() < count) {
+            valueOfDisconnect.add(valueOfDisconnect.size() + 1);
+        }
+
+        while (valueOfDisconnect.size() > count) {
+            valueOfDisconnect.remove(valueOfDisconnect.size() - 1);
+        }
+
+        countOfDisconnect.setItems(valueOfDisconnect);
+
 //        this.cashRegisterSpare.setVisible(false);
 //        this.labelCashRegisterSpare.setVisible(false);
     }
 
-    public void handleStart(ActionEvent event){
+    public void handleStart(ActionEvent event) {
         //start of spawn customers
     }
 
-    public void handleDisconnectCashRegister(ActionEvent event){
+    public void handleDisconnectCashRegister(ActionEvent event) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setContentText("You want disconnect cash register number  " +  cashRegisterDisconnection.getText());
+        alert.setContentText("You want disconnect cash register number  " + countOfDisconnect.getValue());
         alert.showAndWait();
     }
 }
