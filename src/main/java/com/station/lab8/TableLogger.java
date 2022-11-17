b768cbae9b09b7937a2800122b686fc5ea8f2ea1
Saddie <saddie.yy@gmail.com>
2022-11-17 03:07:20 +0200

    fixed logger

--- src/main/java/com/station/lab8/TableLogger.java
+++ src/main/java/com/station/lab8/TableLogger.java
@@ -2,16 +2,20 @@ package com.station.lab8;
 
 import javafx.collections.ObservableList;
 
+import java.util.Random;
+
 public class TableLogger implements ILogger {
 
     ObservableList<LogWrapper> logs;
     public  TableLogger( ObservableList<LogWrapper> logs){
         this.logs = logs;
     }
+
+    static Random random = new Random();
     @Override
     public void log(String msg) {
         var endIndex = msg.indexOf(' ');
-        logs.add(new LogWrapper(Integer.valueOf( msg.substring(0,endIndex)),msg.substring(endIndex,msg.length()) ));
+        logs.add(new LogWrapper(random.nextInt(10000) ,msg.substring(endIndex,msg.length()) ));
 
     }
 }
